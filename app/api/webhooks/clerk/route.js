67b5d67bdfa70a5badf9bade4924a57fabb05d1e
ImChloeLazaro <ckhemskie@gmail.com>
2026-04-10 00:27:19 +1000

    testing adding user data

--- app/api/webhooks/clerk/route.js
+++ app/api/webhooks/clerk/route.js
@@ -1,11 +1,20 @@
 import { verifyWebhook } from '@clerk/nextjs/webhooks';
-
+import {prisma} from '@/lib/prisma';
 export async function POST(req) {
   try {
     const evt = await verifyWebhook(req);
 
     const { id } = evt.data;
     const eventType = evt.type;
+
+    const user = await prisma.user.create({
+      data: {
+        clerkId: id,
+        email: evt.data.email_addresses[0].email_address,
+        name: evt.data.first_name + ' ' + evt.data.last_name,
+        picture: evt.data.profile_image_url,
+      },
+    });
 
     console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
     console.log('Webhook payload:', evt.data);
@@ -14,7 +23,7 @@ export async function POST(req) {
       console.log('userId:', evt.data.id);
     }
 
-    return new Response('Webhook received', { status: 200 });
+    return new Response(JSON.stringify(user), { status: 200 });
   } catch (err) {
     console.error('Error verifying webhook:', err);
     return new Response('Error verifying webhook', { status: 400 });
