fbc8d7047c074052951efe9a971e903e5aba1c70
ImChloeLazaro <ckhemskie@gmail.com>
2026-04-10 00:58:27 +1000

    fix: generate prisma error

--- app/api/webhooks/clerk/route.js
+++ app/api/webhooks/clerk/route.js
@@ -1,5 +1,5 @@
 import { verifyWebhook } from '@clerk/nextjs/webhooks';
-import { PrismaClient } from "../../../../generated/prisma/client"
+import { PrismaClient } from "../../../generated/prisma/client"
 const prisma = new PrismaClient();
 
 export async function POST(req) {
