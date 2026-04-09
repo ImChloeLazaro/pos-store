9ea54dcc061200186cc0b27ab309906f33ae7001
ImChloeLazaro <ckhemskie@gmail.com>
2026-04-10 00:55:13 +1000

    fix: generate prisma error

--- app/api/webhooks/clerk/route.js
+++ app/api/webhooks/clerk/route.js
@@ -1,6 +1,6 @@
 import { verifyWebhook } from '@clerk/nextjs/webhooks';
-// import { PrismaClient } from '@prisma/client';
-// const prisma = new PrismaClient();
+import { PrismaClient } from "../../../../generated/prisma/client"
+const prisma = new PrismaClient();
 
 export async function POST(req) {
   try {
@@ -9,18 +9,18 @@ export async function POST(req) {
     const { id } = evt.data;
     const eventType = evt.type;
 
-    // const user = await prisma.user.create({
-    //   data: {
-    //     clerkId: id,
-    //     email: evt.data.email_addresses[0].email_address,
-    //     name: evt.data.first_name + ' ' + evt.data.last_name,
-    //     picture: evt.data.profile_image_url,
-    //   },
-    // });
+    const user = await prisma.user.create({
+      data: {
+        clerkId: id,
+        email: evt.data.email_addresses[0].email_address,
+        name: evt.data.first_name + ' ' + evt.data.last_name,
+        picture: evt.data.profile_image_url,
+      },
+    });
 
     console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
     console.log('Webhook payload:', evt.data);
-    // console.log('Created user in database:', user);
+    console.log('Created user in database:', user);
     if (evt.type === 'user.created') {
       console.log('userId:', evt.data.id);
     }
